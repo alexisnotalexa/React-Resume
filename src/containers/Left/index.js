d110fb97ccd5706f835836969c4eef49112bebf9
Alexis <alexisko@hawaii.edu>
2018-02-27 18:56:02 -1000

    Almost done with styling, just need to add content and fix button styling

--- src/containers/Left/index.js
+++ src/containers/Left/index.js
@@ -5,16 +5,21 @@ class Left extends Component {
   render() {
     return(
       <div className="left-container">
-        <span className="shaka" role="img" aria-label="shaka">&#x1F919;</span>
-        <h1>Alexis Okamura</h1>
-        <p>Full-Stack Developer based in Honolulu, HI</p>
-        <div className="links">
-          <a href="https://github.com/alexisko"><i className="fa fa-envelope"></i></a>
-          <a href="https://github.com/alexisko"><i className="fa fa-twitter"></i></a>
-          <a href="https://github.com/alexisko"><i className="fa fa-github"></i></a>
-          <a href="https://github.com/alexisko"><i className="fa fa-linkedin"></i></a>
-          <a href="https://github.com/alexisko"><i className="fa fa-codepen"></i></a>
+        <div className="container">
+          <span className="shaka" role="img" aria-label="shaka">&#x1F919;</span>
+          <h1>Alexis Okamura</h1>
+          <p>Full-Stack Developer based in Honolulu, HI</p>
+          <div className="links">
+            <a href="mailto:alexiskokamura@gmail.com"><i className="fas fa-envelope"></i></a>
+            <a href="https://twitter.com/alexisnotalexa"><i className="fab fa-twitter"></i></a>
+            <a href="https://github.com/alexisko"><i className="fab fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/alexis-ko/"><i className="fab fa-linkedin"></i></a>
+            <a href="https://codepen.io/alexisko/"><i className="fab fa-codepen"></i></a>
+          </div>
         </div>
+        <div className="lightblue"></div>
+        <div className="teal"></div>
+        <div className="cadetblue"></div>
       </div>
     );
   }
--- src/containers/Right/index.js
+++ src/containers/Right/index.js
@@ -6,9 +6,17 @@ class Right extends Component {
     return(
       <div className="right-container">
         <section>
+          <h3>SKILLS</h3>
+          <div className="content">
+            <p><strong>LANGUAGES:</strong> Javascript, HTML5, CSS3, Java, SQL</p>
+            <p><strong>LIBRARIES &amp; FRAMEWORKS:</strong> Node, Express, React, Redux, Handlebars, Bootstrap, AngularJS, jQuery, JSTL, PowerMock</p>
+            <p><strong>TOOLS:</strong> Git, Github, Sublime Text, Postman, Gulp, Eclipse, Adobe Photoshop &amp; Lightroom</p>
+          </div>
+        </section>
+        <section>
           <h3>EXPERIENCE</h3>
           <div className="content">
-            <div>
+            <div className="content-container">
               <div className="job">
                 <span className="title">Java Web Developer</span>
                 <span className="date">October 2017 - February 2018</span>
@@ -20,12 +28,12 @@ class Right extends Component {
                 <li>Utilized Java, JSTL, Java Servlets, PowerMock, MySQL, MVC</li>
               </ul>
             </div>
-            <div>
+            <div className="content-container">
               <div className="job">
                 <span className="title">Assistant Web Developer</span>
                 <span className="date">October 2017 - February 2018</span>
               </div>
-              <span>CTAHR Nutrition Education for Wellness Program - Honolulu, HI</span>
+              <span>CTAHR NEW Program - Honolulu, HI</span>
               <ul className="description">
                 <li>Tested and developed ALTRES’ HR Symphony software</li>
                 <li>Worked with a small team that follows agile software development methodologies and scrum</li>
@@ -37,13 +45,36 @@ class Right extends Component {
         <section>
           <h3>PROJECTS</h3>
           <div className="content">
-
+            <div className="content-container">
+              <div className="project">
+                <span className="title">Express Gallery</span>
+                <a href="https://github.com/alexisko/Express-Gallery"><i className="fas fa-asterisk"></i></a>
+              </div>
+              <ul className="description">
+              </ul>
+            </div>
+            <div className="content-container">
+              <div className="project">
+                <span className="title">Help Your City</span>
+                <a href="https://helpyour.city/"><i className="fas fa-asterisk"></i></a>
+              </div>
+              <ul className="description">
+              </ul>
+            </div>
+            <div className="content-container">
+              <div className="project">
+                <span className="title">Free Code Camp Projects</span>
+                <a href="https://codepen.io/alexisko/"><i className="fas fa-asterisk"></i></a>
+              </div>
+              <ul className="description">
+              </ul>
+            </div>
           </div>
         </section>
         <section>
           <h3>EDUCATION</h3>
           <div className="content">
-            <div>
+            <div className="content-container">
               <div className="job">
                 <span className="title">FreeCodeCamp</span>
                 <span className="date">July 2017 - Present</span>
@@ -53,18 +84,18 @@ class Right extends Component {
                 <li>Currently completing over 400 hours of programming</li>
               </ul>
             </div>
-            <div>
+            <div className="content-container">
               <div className="job">
                 <span className="title">Devleague</span>
                 <span className="date">May 2017 - November 2017</span>
               </div>
-              <span>Javascript Web Engineer Track Certificate of Completetion</span>
+              <span>Javascript Web Engineer Track</span>
               <ul className="description">
                 <li>Completed over 800 hours hours of intensive web development programming over a six-month period</li>
                 <li>Participated in AngelHack Global Hackathon with a team and created several Alexa skills for pet owners, July 2017</li>
               </ul>
             </div>
-            <div>
+            <div className="content-container">
               <div className="job">
                 <span className="title">University of Hawaii at Manoa</span>
                 <span className="date">Fall 2011 - Spring 2017</span>
@@ -76,6 +107,9 @@ class Right extends Component {
               </ul>
             </div>
           </div>
+        </section>
+        <section>
+          <button>GET A PDF OF MY RESUME</button>
         </section>
       </div>
     );
