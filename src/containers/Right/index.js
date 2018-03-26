bd4b9510394f7a59a7b819c3877762c9f62b31e7
Alexis <alexisko@hawaii.edu>
2018-03-26 11:36:10 -1000

    Updated text

--- src/containers/Right/index.js
+++ src/containers/Right/index.js
@@ -8,9 +8,9 @@ class Right extends Component {
         <section>
           <h3>SKILLS</h3>
           <div className="content">
-            <p><strong>LANGUAGES:</strong> Javascript (ES5&amp;ES6), HTML5, CSS3/SCSS, Java, SQL</p>
-            <p><strong>LIBRARIES &amp; FRAMEWORKS:</strong> React, Redux, Angular, Node, Express, Handlebars, Bootstrap, jQuery, JSTL, PowerMock</p>
-            <p><strong>TOOLS:</strong> Git &amp; Github, Sublime Text, Visual Studio Code, Postman, Gulp, Eclipse, Adobe Photoshop, Dreamweaver, Lightroom</p>
+            <p><strong>LANGUAGES:</strong> Javascript (ES5&amp;ES6), HTML5, CSS3/SCSS, Java, SQL/NoSQL (MySQL, PostgreSQL, MongoDB, Redis)</p>
+            <p><strong>LIBRARIES &amp; FRAMEWORKS:</strong> React, Redux, Angular, Node, Express, Handlebars, Bootstrap, jQuery, JSTL, PowerMock/EasyMock</p>
+            <p><strong>TOOLS:</strong> Git &amp; Github, Node/NPM, Sublime Text, Postman, Gulp, Eclipse, Adobe Photoshop, Dreamweaver, Lightroom</p>
           </div>
         </section>
         <section>
@@ -23,9 +23,10 @@ class Right extends Component {
               </div>
               <span>Altres - Honolulu, HI</span>
               <ul className="description">
-                <li>Tested and developed ALTRES’ HR Symphony software</li>
-                <li>Worked with a small team that follows agile software development methodologies</li>
-                <li>Utilized Java, JSTL, Java Servlets, PowerMock, MySQL</li>
+                <li>Developed a feature using Java, JSTL, and Java Servlets that allows users to import/export Excel documents to track employee costing rates and update any changes in the database</li>
+                <li>Created and updated front end features utilizing JSTL, jQuery, and Javascript</li>
+                <li>Tested existing software features and functions using PowerMock/EasyMock</li>
+                <li>Worked on a team that follows agile software development methodologies</li>
               </ul>
             </div>
             <div className="content-container">
@@ -35,9 +36,8 @@ class Right extends Component {
               </div>
               <span>CTAHR NEW Program - Honolulu, HI</span>
               <ul className="description">
-                <li>Contributed to website maintenance, testing, and development</li>
-                <li>Converted the CTAHR website to be ADA compliant</li>
-                <li>Assisted in troubleshooting hardware and software issues</li>
+                <li>Responsible for website maintenance by testing existing features and updating information throughout the website along with troubleshooting any software issues</li>
+                <li>Assisted in converting the CTAHR website to be ADA compliant</li>
               </ul>
             </div>
           </div>
@@ -50,7 +50,8 @@ class Right extends Component {
                 <span className="title">Table for Two - AT&amp;T Hackathon 2018</span>
                 <a href="https://github.com/alexisnotalexa/ATTHackathon2018"><i className="fas fa-asterisk"></i></a>
               </div>
-              <p>Designed an app to connect you to someone nearby so that you won’t have to eat alone.  Heavily focused on curating an experience for users that’s fast, delightful, and easy to use.</p>
+              <p>Designed a React Native application for the 2018 AT&T Hackathon that connects two users based on restaurant interests and current location</p>
+              <p>Created a simple and efficient user experience by planning out the application flow through wireframes and focusing on a minimalistic design</p>
               <p><strong>Technologies:</strong> React Native, Express, Node, MongoDB</p>
             </div>
             <div className="content-container">
@@ -58,24 +59,17 @@ class Right extends Component {
                 <span className="title">Free Code Camp Projects</span>
                 <a href="https://codepen.io/alexisnotalexa/"><i className="fas fa-asterisk"></i></a>
               </div>
-              <p>Currently completing various different front-end programming projects</p>
+              <p>Developed several front-end projects utilizing HTML5/CSS3, Javascript, jQuery, Bootstrap, CSS animations, and various APIs</p>
               <p><strong>Technologies:</strong> HTML5/CSS3, jQuery, Javascript, Bootstrap</p>
-            </div>
-            <div className="content-container">
-              <div className="project">
-                <span className="title">Express Gallery</span>
-                <a href="https://github.com/alexisnotalexa/Express-Gallery"><i className="fas fa-asterisk"></i></a>
-              </div>
-              <p>Built a full-stack gallery web application based on a given design spec.</p>
-              <p><strong>Technologies:</strong> Node, Express, PostgreSQL, SCSS, Gulp, Redis, Handlebars, HTML5/CSS3</p>
             </div>
             <div className="content-container">
               <div className="project">
                 <span className="title">Help Your City</span>
                 <a href="https://helpyour.city/"><i className="fas fa-asterisk"></i></a>
               </div>
-              <p>Developed a web app with a team to help connect people in need to others that can help them during natural disasters without needing to communicate to an overloaded network of first responders.</p>
-              <p><strong>Technologies:</strong> Node, React, Redux, PostgreSQL, Express, JWT, Twilio, Sequelize</p>
+              <p>Developed a full-stack web application in a team to connect people quickly during natural disasters without the user needing to communicate to emergency services</p>
+              <p>Responsible for the design of the application while managing and delegating tasks to the team</p>
+              <p><strong>Technologies:</strong> React, Redux, PostgreSQL, Express, JWT, Twilio, Sequelize</p>
             </div>
           </div>
         </section>
@@ -100,9 +94,8 @@ class Right extends Component {
               </div>
               <span>Javascript Web Engineer Track</span>
               <ul className="description">
-                <li>Completed over 800 hours of intensive web development programming over a six-month period</li>
-                <li>Devleague's curriculum is focused on teaching popular coding languages and technologies such as Node, AngularJS, React, Redux, SASS, Gulp, PostgreSQL, and MongoDB to name a few</li>
-                <li>Participated in AngelHack Global Hackathon with a team and created several Alexa skills for pet owners, July 2017</li>
+                <li>Completed over 800 hours in an intensive coding boot camp focused on learning and utilizing modern web development languages and best practices for developing applications built for both mobile and web platforms</li>
+                <li>Participated in team for the 2017 AngelHack Global Hackathon to create Alexa skills for pet owners and designed a landing page for the product</li>
               </ul>
             </div>
             <div className="content-container">
@@ -111,10 +104,6 @@ class Right extends Component {
                 <span className="date">Fall 2011 - Spring 2017</span>
               </div>
               <span>BA in Computer Science + Certificate in Japanese Language</span>
-              <ul className="description">
-                <li>Japanese Literature Poetry Contest Winner, Spring 2012</li>
-                <li>Dean’s Honor List, Fall 2012</li>
-              </ul>
             </div>
           </div>
         </section>
